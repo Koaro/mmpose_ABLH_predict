9e8fce69bc6abfb227e6d738bc2c7008d7fed97c
Koaro <44613618+Koaro@users.noreply.github.com>
2023-07-31 13:25:46 +0800

    Add files via upload

---
+++ mmpose/config/_base_/coco.py
@@ -0,0 +1,24 @@
+dataset_info = dict(
+    dataset_name='coco',
+    paper_info=dict(
+        author='Lin, Tsung-Yi and Maire, Michael and '
+        'Belongie, Serge and Hays, James and '
+        'Perona, Pietro and Ramanan, Deva and '
+        r'Doll{\'a}r, Piotr and Zitnick, C Lawrence',
+        title='Microsoft coco: Common objects in context',
+        container='European conference on computer vision',
+        year='2014',
+        homepage='http://cocodataset.org/',
+    ),
+    keypoint_info={
+        0:
+        dict(name='nose', id=0, color=[51, 153, 255], type='upper', swap='')
+    },
+    skeleton_info={
+    },
+    joint_weights=[
+        1.
+    ],
+    sigmas=[
+        0.026
+    ])
